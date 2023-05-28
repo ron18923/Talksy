ad65e9b485e5cc3f19625068efc4f30f7a91334c
ron18923 <ronron18923@gmail.com>
2023-05-28 22:43:01 +0300

    added basic bottom navigation bar for the ChatPage.kt

--- app/src/main/java/com/example/talksy/compose/ChatPage.kt
+++ app/src/main/java/com/example/talksy/compose/ChatPage.kt
@@ -1,14 +1,26 @@
 package com.example.talksy.compose
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import com.example.talksy.UserViewModel
 import com.example.talksy.compose.destinations.OnBoardingDestination
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Destination(start = true)
 @Composable
 fun ChatPage(
@@ -16,6 +28,21 @@ fun ChatPage(
     navigator: DestinationsNavigator?,
     userViewModel: UserViewModel
 ) {
-    if(userViewModel.isSignedIn()) navigator?.navigate(OnBoardingDestination)
+    if (userViewModel.isSignedIn()) navigator?.navigate(OnBoardingDestination)
 
+    Scaffold(bottomBar = { NavigationBar(){
+        //TODO implement it later correctly.
+        val items = listOf("Songs", "Artists", "Playlists")
+        var selectedItem = remember { mutableStateOf(0) }
+        items.forEachIndexed { index, item ->
+            NavigationBarItem(
+                icon = { Icon(Icons.Filled.Favorite, contentDescription = item) },
+                label = { Text(item) },
+                selected = selectedItem.value == index,
+                onClick = { selectedItem.value = index }
+            )
+        }
+    }}) {
+        Box(modifier = modifier.padding(it))
+    }
 }
